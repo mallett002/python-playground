d40b6eeafb94c069e7aac259e0583dd4d4debd8e
William Mallett <mallett002@gmail.com>
2024-10-22 06:12:56 -0500

    lists and tuples

--- codecademy/lists-tuples.py
+++ codecademy/lists-tuples.py
@@ -1,3 +1,31 @@
+def tuples():
+    my_tupe = ('hi', 123, 'fooey', 45.9)
+    one_item_tuple = (1,) # needs a comma at the end
+    nums = (3, 9, 10, 1, 30, 4)
+    colors = ('orange', 'blue', 'red', 'green')
+
+    # accessig items
+    print(my_tupe[3]) # --> 45.9
+    print(my_tupe[1:3]) # --> ('fooey', 45.9)
+
+    # len(): gets the length
+    print(len(my_tupe))
+
+    # max(): gets tuple's maximum value
+    print(max(nums))
+    print(max(colors)) # sorted alphabetically ascending
+
+    # min(): gets tuple's min value
+    print(min(nums))
+    print(min(colors)) # sorted alphabetically ascending
+
+    # index(): finds the index of the provided arg
+    print(colors.index('red')) # --> 2
+
+    # count(): Gets number of occurances of the tuple
+    more_colors = ('red', 'blue', 'red')
+    print(more_colors.count('red')) # --> 2
+
 
 def lists():
     lst1 = ['abc', 123, 'def', 10.5, 62, ['g', 'h', 'i']]
@@ -29,6 +57,7 @@ def lists():
 
 def main():
     # lists()
+    tuples()
 
 if __name__ == '__main__':
     main()
